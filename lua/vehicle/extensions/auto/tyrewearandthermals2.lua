fc1db8fbfcabfe5df7c601970270ae422d89daea
Todd <todd.griffin61@outlook.com>
2025-01-31 22:27:27 +0000

    fixed indentation

--- lua/vehicle/extensions/auto/tyrewearandthermals2.lua
+++ lua/vehicle/extensions/auto/tyrewearandthermals2.lua
@@ -14,151 +14,155 @@ tyres = {} -- left global so other mods can interact with it
 
 
 function TWT.changeTyres(resetTemp)
-	if resetTemp then
-		for _, tyre in pairs(tyres) do tyre:changeTyre(resetTemp) end
-	else
-		for _, tyre in pairs(tyres) do tyre:changeTyre(tyre.idealTemp) end
-	end
+    if resetTemp then
+        for _, tyre in pairs(tyres) do tyre:changeTyre(resetTemp) end
+    else
+        for _, tyre in pairs(tyres) do tyre:changeTyre(tyre.idealTemp) end
+    end
 end
 
 local function setEnvironmentTemperature()
-	TWT.env_temp = obj:getEnvTemperature() - 273.15
+    TWT.env_temp = obj:getEnvTemperature() - 273.15
 end
 
 local function getGroundModelData(id)
-	local materials, materialsMap = particles.getMaterialsParticlesTable()
-	local matData = materials[id] or {}
-	local name = matData.name or "DOESNT EXIST"
-	local data = TWT.groundModels[name] or { staticFrictionCoefficient = 1, slidingFrictionCoefficient = 1 }
-	return name, data
+    local materials, materialsMap = particles.getMaterialsParticlesTable()
+    local matData = materials[id] or {}
+    local name = matData.name or "DOESNT EXIST"
+    local data = TWT.groundModels[name] or { staticFrictionCoefficient = 1, slidingFrictionCoefficient = 1 }
+    return name, data
 end
 
 local function getMapNormalUnderWheel(wheelID)
-	local vectorUp = obj:getDirectionVectorUp()
-	local localVectNode1 = obj:getNodePosition(wheels.wheelRotators[wheelID].node1)
-	local localVectNode2 = obj:getNodePosition(wheels.wheelRotators[wheelID].node2)
-	local vectorWheelForward = (localVectNode2 - localVectNode1):cross(vectorUp)
-	local vectorWheelUp = vectorWheelForward:cross(localVectNode2 - localVectNode1)
-	local surfaceNormal = mapmgr.surfaceNormalBelow(
-		obj:getPosition() + (localVectNode2 + localVectNode1) / 2 -
-		wheels.wheelRotators[wheelID].radius * vectorWheelUp:normalized(), 0.1
-	)
-	return surfaceNormal
+    local vectorUp = obj:getDirectionVectorUp()
+    local localVectNode1 = obj:getNodePosition(wheels.wheelRotators[wheelID].node1)
+    local localVectNode2 = obj:getNodePosition(wheels.wheelRotators[wheelID].node2)
+    local vectorWheelForward = (localVectNode2 - localVectNode1):cross(vectorUp)
+    local vectorWheelUp = vectorWheelForward:cross(localVectNode2 - localVectNode1)
+    local surfaceNormal = mapmgr.surfaceNormalBelow(
+        obj:getPosition() + (localVectNode2 + localVectNode1) / 2 -
+        wheels.wheelRotators[wheelID].radius * vectorWheelUp:normalized(), 0.1
+    )
+    return surfaceNormal
 end
 
 local function getWheelCamberToGround(wheelID)
-	local localVectNode1 = obj:getNodePosition(wheels.wheelRotators[wheelID].node1)
-	local localVectNode2 = obj:getNodePosition(wheels.wheelRotators[wheelID].node2)
-	local surfaceNormal = getMapNormalUnderWheel(wheelID)
-	local camber = 90 -
-	math.deg(math.acos((localVectNode2 - localVectNode1):normalized():dot(surfaceNormal:normalized())))
-	return camber
+    local localVectNode1 = obj:getNodePosition(wheels.wheelRotators[wheelID].node1)
+    local localVectNode2 = obj:getNodePosition(wheels.wheelRotators[wheelID].node2)
+    local surfaceNormal = getMapNormalUnderWheel(wheelID)
+    local camber = 90 -
+    math.deg(math.acos((localVectNode2 - localVectNode1):normalized():dot(surfaceNormal:normalized())))
+    return camber
 end
 
+local function getNodeForcePerpendicularToGround(nodeID)
+    local nodeForce = obj:getNodeForceVector(nodeID)
+end
 
 local function generateStream(tyres)
-	local stream = { data = {} }
-	for _, tyre in pairs(tyres) do
-		table.insert(stream.data, {
-			name = tyre.name,
-			temps = tyre.matNodes.l1.temperature,
-			working_temp = 85,
-			condition_zones = tyre.treadConditions,
-			camber = tyre.camber_to_ground,
-			weightOnWheel = tyre:hasWeightOnWheel()
-		})
-	end
-	return stream
+    local stream = { data = {} }
+    for _, tyre in pairs(tyres) do
+        table.insert(stream.data, {
+            name = tyre.name,
+            temps = tyre.matNodes.l1.temperature,
+            working_temp = 85,
+            condition_zones = tyre.treadConditions,
+            camber = tyre.camber_to_ground,
+            weightOnWheel = tyre:hasWeightOnWheel()
+        })
+    end
+    return stream
 end
 
 local dummyStream = {
-	data = {
-		name = "not a real tyre",
-		temps = { TWT.env_temp, TWT.env_temp },
-		working_temp = TWT.env_temp,
-		condition_zones = { 100 },
-		camber = 0
-	}
+    data = {
+        name = "not a real tyre",
+        temps = { TWT.env_temp, TWT.env_temp },
+        working_temp = TWT.env_temp,
+        condition_zones = { 100 },
+        camber = 0
+    }
 }
 
 local function getWheelsAndNamesPositions(wheels)
-	-- INFO: unused, will be used to standardise tyre names and
-	-- their position relative to the vehicle and its orentation
-	-- so the UI can display them
-	local wheelNamesAndPositions = {}
-	for i, v in pairs(wheels.wheelRotators) do
-		local localVectNode1 = obj:getNodePosition(wheels.wheelRotators[i].node2)
-		local wheelName = wheels.wheelRotators[i].name
-		wheelNamesAndPositions[wheelName] = localVectNode1
-	end
-	return wheelNamesAndPositions
+    -- INFO: unused, will be used to standardise tyre names and
+    -- their position relative to the vehicle and its orentation
+    -- so the UI can display them
+    local wheelNamesAndPositions = {}
+    for i, v in pairs(wheels.wheelRotators) do
+        local localVectNode1 = obj:getNodePosition(wheels.wheelRotators[i].node2)
+        local wheelName = wheels.wheelRotators[i].name
+        wheelNamesAndPositions[wheelName] = localVectNode1
+    end
+    return wheelNamesAndPositions
 end
 
 local oneSecondTimer = 0
 local function updateGFX(dt)
-	-- INFO: unused, will be used to standardise tyre names and
-	-- their position relative to the vehicle and its orentation
-	-- so the UI can display them
-	-- local vehicleVectorForward = obj:getDirectionVector()
-	-- local wheelNamesAndPositions = getWheelsAndNamesPositions(wheels)
-	-- local standarisedNames = standardiseTyreNames.standardise(wheelNamesAndPositions, vehicleVectorForward)
+    setEnvironmentTemperature()
+    -- INFO: unused, will be used to standardise tyre names and
+    -- their position relative to the vehicle and its orentation
+    -- so the UI can display them
+    -- local vehicleVectorForward = obj:getDirectionVector()
+    -- local wheelNamesAndPositions = getWheelsAndNamesPositions(wheels)
+    -- local standarisedNames = standardiseTyreNames.standardise(wheelNamesAndPositions, vehicleVectorForward)
 
-	oneSecondTimer = oneSecondTimer + dt
-	if wheels == nil or wheels.wheelRotators == nil or wheels.wheelRotators[0] == nil then
-		-- if the above statement is true, the vehicle most likely doesn't have tyres
-		gui.send("tyrewearandthermals2", dummyStream)
-		return
-	end
+    oneSecondTimer = oneSecondTimer + dt
+    if wheels == nil or wheels.wheelRotators == nil or wheels.wheelRotators[0] == nil then
+        -- if the above statement is true, the vehicle most likely doesn't have tyres
+        gui.send("tyrewearandthermals2", dummyStream)
+        return
+    end
 
-	for i, tyre in pairs(tyres) do
-		local groundModelName, groundModel = getGroundModelData(wheels.wheelRotators[i].contactMaterialID1)
-		local wheelload = wheels.wheelRotators[i].downForce
-		local wheelname = wheels.wheelRotators[i].name
-		tyre:update(
-			dt,
-			getWheelCamberToGround(i),
-			{
-				env_temp = TWT.env_temp,
-				load = wheels.wheelRotators[i].downForceRaw,
-				angularVel = wheels.wheelRotators[i].angularVelocity,
-				propulsionTorque = wheels.wheelRotators[i].propulsionTorque,
-				brakingTorque = wheels.wheelRotators[i].brakingTorque,
-				lastTorque = wheels.wheelRotators[i].lastTorque
-			})
-	end
+    for i, tyre in pairs(tyres) do
+        local groundModelName, groundModel = getGroundModelData(wheels.wheelRotators[i].contactMaterialID1)
+        local wheelload = wheels.wheelRotators[i].downForce
+        local wheelname = wheels.wheelRotators[i].name
+        tyre:update(
+            dt,
+            getWheelCamberToGround(i),
+            {
+                env_temp = TWT.env_temp,
+                load = wheels.wheelRotators[i].downForceRaw,
+                angularVel = wheels.wheelRotators[i].angularVelocity,
+                propulsionTorque = wheels.wheelRotators[i].propulsionTorque,
+                brakingTorque = wheels.wheelRotators[i].brakingTorque,
+                lastTorque = wheels.wheelRotators[i].lastTorque
+            })
+    end
 
-	if oneSecondTimer >= 1 then
-		TWT.changeTyres()
-		-- TWT.changeTyres(TWT.env_temp)
-		oneSecondTimer = oneSecondTimer % 1 -- Loops every 1 seconds
-	end
+    if oneSecondTimer >= 1 then
+        TWT.changeTyres()
+        -- TWT.changeTyres(TWT.env_temp)
+        oneSecondTimer = oneSecondTimer % 1 -- Loops every 1 seconds
+    end
 
-	local stream = generateStream(tyres)
-	gui.send("tyrewearandthermals2", stream)
+    local stream = generateStream(tyres)
+    gui.send("tyrewearandthermals2", stream)
 end
 
 local function generateModTyres()
-	for i, wheel in pairs(wheels.wheelRotators) do
-		tyres[wheel.wheelID] = useTyre.new(wheel.name, wheel.wheelID, wheel.wheelDir,
-			{ tyreMass = 10, idealTemp = 85, wear_rate = 0.005, wheel.tireWidth, isPreheated = true })
-	end
+    for i, wheel in pairs(wheels.wheelRotators) do
+        tyres[wheel.wheelID] = useTyre.new(wheel.name, wheel.wheelID, wheel.wheelDir,
+            { tyreMass = 10, idealTemp = 85, wear_rate = 0.005, wheel.tireWidth, isPreheated = true })
+    end
 end
 
 local function onReset()
-	setEnvironmentTemperature()
-	generateModTyres()
+    setEnvironmentTemperature()
+    generateModTyres()
 
-	obj:queueGameEngineLua("if tyrewearandthermals2 then tyrewearandthermals2.setBrakeSettings() end")
-	obj:queueGameEngineLua("if tyrewearandthermals2 then tyrewearandthermals2.setTyreWearAndThermalVariables() end")
+    obj:queueGameEngineLua("if tyrewearandthermals2 then tyrewearandthermals2.setBrakeSettings() end")
+    obj:queueGameEngineLua("if tyrewearandthermals2 then tyrewearandthermals2.setTyreWearAndThermalVariables() end")
 end
 
 local function onInit()
-	setEnvironmentTemperature()
-	generateModTyres()
+    setEnvironmentTemperature()
+    generateModTyres()
 
-	obj:queueGameEngineLua("if tyrewearandthermals2 then tyrewearandthermals2.setGroundModels() end")
-	obj:queueGameEngineLua("if tyrewearandthermals2 then tyrewearandthermals2.setBrakeSettings() end")
-	obj:queueGameEngineLua("if tyrewearandthermals2 then tyrewearandthermals2.setTyreWearAndThermalVariables() end")
+    obj:queueGameEngineLua("if tyrewearandthermals2 then tyrewearandthermals2.setGroundModels() end")
+    obj:queueGameEngineLua("if tyrewearandthermals2 then tyrewearandthermals2.setBrakeSettings() end")
+    obj:queueGameEngineLua("if tyrewearandthermals2 then tyrewearandthermals2.setTyreWearAndThermalVariables() end")
 end
 
 M.updateGFX = updateGFX
